6cbd3f1369ef6ba166434aa43f217cd10e83b828
Matt Williams <matt.williams@exacttarget.com>
2014-08-21 12:00:54 -0500

    Initial commit

---
+++ utils/test-template.js
@@ -0,0 +1,13 @@
+define( function( require ) {
+	'use strict';
+
+	// the require statement below may need to be altered
+	// based on your require paths configuration.
+	// var ObjectToTest = require( '%%src%%' );
+
+	describe( '%%src%%', function() {
+		it( 'should have unit tests', function() {
+			expect( false ).to.equal( true );
+		});
+	});
+});
